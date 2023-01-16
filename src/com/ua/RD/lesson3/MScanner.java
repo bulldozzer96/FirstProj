96964c106869e87190aba4b14bd674ee727fb884
bulldozzer96 <bulldozzer96@gmail.com>
2023-01-16 20:26:32 +0200

    lesson3

---
+++ src/com/ua/RD/lesson3/MScanner.java
@@ -0,0 +1,12 @@
+package com.ua.RD.lesson3;
+
+import java.util.Scanner;
+
+public class MScanner {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner() ;
+        System.out.println();
+
+    }
+}
