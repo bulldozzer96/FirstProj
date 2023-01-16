24220d124f7fbdf97352aa52b696b183cdaca60e
bulldozzer96 <bulldozzer96@gmail.com>
2023-01-16 20:32:03 +0200

    lesson3

--- src/com/ua/RD/lesson3/MScanner.java
+++ src/com/ua/RD/lesson3/MScanner.java
@@ -5,8 +5,9 @@ import java.util.Scanner;
 public class MScanner {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner() ;
-        System.out.println();
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter number: ");
+        int fromInput = scanner.nextInt();
+        System.out.println("Your input: " + fromInput);
     }
 }
