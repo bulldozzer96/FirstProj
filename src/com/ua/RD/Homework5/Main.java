f2f13d3d296641b881a8c546a1ef02c8810f7192
bulldozzer96 <bulldozzer96@gmail.com>
2023-01-24 22:23:42 +0200

    Homework5

--- src/com/ua/RD/Homework5/Main.java
+++ src/com/ua/RD/Homework5/Main.java
@@ -2,18 +2,25 @@ package com.ua.RD.Homework5;
 
 public class Main {
     public static void main(String[] args) {
-        int a = 123;
-        int b = 124;
+        int a = 10;
+        int b = 12;
 
-        boolean GreaterA = a > b && a != b;
-        boolean GreaterB = a < b && a != b;
+        boolean aGreaterPair = a > b && a != b && a % 2 == 0;
+        boolean bGreaterPair = a < b && a != b&& b % 2 == 0;
+        boolean aGreaterNonPair = a > b && a != b && a % 2 != 0;
+        boolean bGreaterNonPair = a < b && a != b&& b % 2 != 0;
 
-        if (GreaterA) {
-            System.out.println("A is greater than B");
-        } else if (GreaterB) {
-            System.out.println("B is greater than A");
-        } else {
+
+        if (aGreaterPair) {
+            System.out.println("A is pair and greater than B");
+        } else if (bGreaterPair) {
+            System.out.println("B is pair greater than A");
+        } else if (aGreaterNonPair){
+            System.out.println("A is`t pair and greater than B");
+        } else if (bGreaterNonPair){
+            System.out.println("B is`t pair and greater than A");
+        }else
             System.out.println("They are equal");
-        }
     }
 }
+//   System.out.println("They are equal");
