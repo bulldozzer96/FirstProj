8cdb5fbed44c3200b5ac73253ccec2308b3a7939
bulldozzer96 <bulldozzer96@gmail.com>
2023-01-31 20:04:52 +0200

    Homework7

--- src/com/ua/RD/Homework7/main.java
+++ src/com/ua/RD/Homework7/main.java
@@ -2,37 +2,36 @@ package com.ua.RD.Homework7;
 
 public class main {
     public static void main(String[] args) {
-        int base = 3;
-        int step = 4;
+        int base = 4; //базове число
+        int step = 3; //число степені
+        String symbol = "2"; //символ для виведення
         stepen(base, step);
-       // stepRec(base, step);
-        System.out.println("Число "+base+" у степені "+step+" дорівнює "+stepen(base, step)); //виведення результату
-       // System.out.println("Число "+base+" у степені "+step+" дорівнює "+stepRec(base, step)); //виведення результату
-
-        String symbol = "*";
-        printStars(step);
-        //printSymbol(step, symbol);
-
+        // stepRec(base, step);
+        System.out.print("Число " + base + " у степені " + step + " дорівнює " + stepen(base, step)); //виведення результату
+        // System.out.println("Число "+base+" у степені "+step+" дорівнює "+stepRec(base, step)); //виведення результату
+        System.out.println(" ");
+        printStars(stepen(base, step));
+        printSymbol(step, symbol);
     }
 
     //1. Створити метод який повертає куб заданого числа
 
-    static int stepen(int value1, int stepNumber1) {
+    static int stepen(int number1, int stepNumber1) {
         int result = 1;
         for (int i = 1; i <= stepNumber1; i++) {
-            result = result * value1;
+            result = result * number1;
         }
         return result;
     }
 
     //степінь із застусуванням рекурсії
-//    static int stepRec(int number, int stepNumber) {
-//        if (stepNumber == 1) {
-//            return number;
-//        } else {
-//            return number * stepRec(number, stepNumber - 1);
-//        }
-//    }
+    //    static int stepRec(int number, int stepNumber) {
+    //        if (stepNumber == 1) {
+    //            return number;
+    //        } else {
+    //            return number * stepRec(number, stepNumber - 1);
+    //        }
+    //    }
 
     //2. Метод повинен вивести зірочку стільки разів, скільки передав користувач у метод
 
@@ -43,6 +42,12 @@ public class main {
         System.out.println();
     }
     //3. Метод повинен вивести переданий символ стільки разів, скільки передав користувач у метод
+
+    static void printSymbol(int number, String symbol) {
+        for (int i = 0; i < number; i++) {
+            System.out.print(symbol);
+        }
+    }
 
 }
 
