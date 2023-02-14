2157a6ba2da2fd87a115a77cc422f0559949951d
bulldozzer96 <bulldozzer96@gmail.com>
2023-02-14 18:58:46 +0200

    Homework11

--- src/com/ua/RD/Homework11/Main.java
+++ src/com/ua/RD/Homework11/Main.java
@@ -4,26 +4,23 @@ package com.ua.RD.Homework11;
 public class Main {
     public static void main(String[] args) {
 
-        Light l1 = new Light("", "", "", "");
-        l1.setType("Pickup");
-        l1.setBrand("FORD");
-        l1.setColor("Red");
-        l1.setEngine("V8");
+        Light civil = new Light("", "", "", "");
+        civil.setType("Pickup");
+        civil.setBrand("FORD");
+        civil.setColor("Red");
+        civil.setEngine("V8");
+        System.out.println(civil);
+        civil.drive();
 
 
-        Heavy h1 = new Heavy("", "", "", "");
-        h1.setType("Truck");
-        h1.setBrand("VOLVO");
-        h1.setCapacity("50T");
-        h1.setPurpose("quarry");
 
-
-        System.out.println(l1);
-        System.out.println(h1);
-
-
-        l1.drive();
-        h1.drive();
+        Heavy cargo = new Heavy("", "", "", "");
+        cargo.setType("Truck");
+        cargo.setBrand("VOLVO");
+        cargo.setCapacity("50T");
+        cargo.setPurpose("quarry");
+        System.out.println(cargo);
+        cargo.drive();
 
 
     }
