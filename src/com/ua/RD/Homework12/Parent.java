357425ae24a7bfeb1990c5b7d60785dd27903869
bulldozzer96 <bulldozzer96@gmail.com>
2023-02-17 18:50:24 +0200

    Homework12

---
+++ src/com/ua/RD/Homework12/Parent.java
@@ -0,0 +1,16 @@
+package com.ua.RD.Homework12;
+
+public class Parent {
+
+
+    static {
+        System.out.println("1");
+    }
+    {
+        System.out.println("5");
+    }
+
+    {
+        System.out.println("6");
+    }
+}
