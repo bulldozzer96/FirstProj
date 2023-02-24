6f003c043f36cc559fd1b5538cab7a0d4b34d77b
bulldozzer96 <bulldozzer96@gmail.com>
2023-02-24 22:30:22 +0200

    Homework 14

---
+++ src/com/ua/RD/Homewrok14/Human.java
@@ -0,0 +1,8 @@
+package com.ua.RD.Homewrok14;
+
+
+
+public abstract class Human {
+    public abstract String discriptOut();
+
+}
---
+++ src/com/ua/RD/Homewrok14/Student.java
@@ -0,0 +1,45 @@
+package com.ua.RD.Homewrok14;
+
+public class Student extends Human {
+    private int course;
+    private String name;
+    private String surname;
+
+    public Student() {
+    }
+
+    public Student(int course, String name, String surname) {
+        this.course = course;
+        this.name = name;
+        this.surname = surname;
+    }
+
+    public int getCourse() {
+        return course;
+    }
+
+    public void setCourse(int course) {
+        this.course = course;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+
+    public String discriptOut() {
+        return "Course: " + course + " name: " + name + " surname: " + surname;
+    }
+}
