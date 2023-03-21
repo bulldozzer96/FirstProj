8038680a83f98dce227a909995722b7714abb640
bulldozzer96 <bulldozzer96@gmail.com>
2023-03-21 19:18:17 +0200

    Homework 20

--- src/com/ua/RD/Homework20/Main.java
+++ src/com/ua/RD/Homework20/Main.java
@@ -14,16 +14,16 @@ public class Main {
 
 
         Map<String, List<String>> dictionary = new HashMap<>();
-        dictionary.put("рушниця", List.of("in english: rifle", "auf deutsch: gewehr", "銃"));
-        dictionary.put("паляниця", List.of("in english: loaf", "auf deutsch: laib", "パン"));
-        dictionary.put("полуниця", List.of("in english: strawberry", "auf deutsch: erdbeere", "いちご"));
-        dictionary.put("потяг", List.of("in english: train", "auf deutsch: der zug", "列車"));
-        dictionary.put("автомобіль", List.of("in english: car", "auf deutsch: auto", "車"));
-        dictionary.put("поле", List.of("in english: field", "auf deutsch: feld", "分野"));
-        dictionary.put("сонце", List.of("in english: sun", "auf deutsch: sonne", "太陽"));
-        dictionary.put("червоний", List.of("in english: red", "auf deutsch: rot", "赤"));
-        dictionary.put("монітор", List.of("in english: monitor", "auf deutsch: monitor", "モニター"));
-        dictionary.put("двері", List.of("in english: door", "auf deutsch: tür", "ドア"));
+        dictionary.put("рушниця", List.of("in english: rifle", "auf deutsch: gewehr", "日本語で: 銃"));
+        dictionary.put("паляниця", List.of("in english: loaf", "auf deutsch: laib", "日本語で: パン"));
+        dictionary.put("полуниця", List.of("in english: strawberry", "auf deutsch: erdbeere", "日本語で: いちご"));
+        dictionary.put("потяг", List.of("in english: train", "auf deutsch: der zug", "日本語で: 列車"));
+        dictionary.put("автомобіль", List.of("in english: car", "auf deutsch: auto", "日本語で: 車"));
+        dictionary.put("поле", List.of("in english: field", "auf deutsch: feld", "日本語で: 分野"));
+        dictionary.put("сонце", List.of("in english: sun", "auf deutsch: sonne", "日本語で: 太陽"));
+        dictionary.put("червоний", List.of("in english: red", "auf deutsch: rot", "日本語で: 赤"));
+        dictionary.put("монітор", List.of("in english: monitor", "auf deutsch: monitor", "日本語で: モニター"));
+        dictionary.put("двері", List.of("in english: door", "auf deutsch: tür", "日本語で: ドア"));
 
         System.out.println(dictionary.get(fromInput));
 
