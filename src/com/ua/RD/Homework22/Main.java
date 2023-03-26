d895f15e5d8a2155d98532bd9ff6e38546869ba2
bulldozzer96 <bulldozzer96@gmail.com>
2023-03-26 22:15:34 +0300

    Homework 22

---
+++ src/com/ua/RD/Homework22/Main.java
@@ -0,0 +1,28 @@
+package com.ua.RD.Homework22;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) {
+
+        List<Student> students = new ArrayList<>();
+
+
+        students.add(new Student("Ivan", 30.5));
+        students.add(new Student("Andriy", 20.9));
+        students.add(new Student("Viktor", 19.2));
+
+
+        System.out.println(students);
+
+        students.sort(Comparator.comparing(Student::getAvgScore));
+        System.out.println(students);
+
+        students.sort(Comparator.comparing(Student::getName));
+        System.out.println(students);
+
+
+    }
+}
