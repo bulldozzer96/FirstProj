8c2598ad3a769563c24fef67081cdcce56b676c1
bulldozzer96 <bulldozzer96@gmail.com>
2023-04-10 20:55:27 +0300

    lesson26

--- src/com/ua/RD/lesson26/Main.java
+++ src/com/ua/RD/lesson26/Main.java
@@ -1,8 +1,37 @@
 package com.ua.RD.lesson26;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+//        Scanner scanner = null;
+//        File file = new File("1.txt");
+//        try {
+//            scanner = new Scanner(file);
+//            System.out.println(scanner.nextLine());
+//
+//        } catch (FileNotFoundException e) {
+//            throw new RuntimeException(e);
+//
+//        } finally {
+//            if (scanner != null) {
+//                scanner.close();
+//            }
+//
+//
+//        }
+//
+//    }
 
+        try (Scanner scanner = new Scanner(new File("1.txt"))) { // AutoClosable Scanner, в дужках до try
+            System.out.println(scanner.nextLine());
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+
+        }
 
     }
 }
