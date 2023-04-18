4b084ef5c2fb1bdeeca1bfab16ff725aeadbd24e
bulldozzer96 <bulldozzer96@gmail.com>
2023-04-18 20:58:50 +0300

    Homework28

---
+++ src/com/ua/RD/Homework28/Main.java
@@ -0,0 +1,27 @@
+package com.ua.RD.Homework28;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+//import java.util.List;
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Files.deleteIfExists(Path.of("Homework28.txt"));
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter smth: ");
+        String txt = scanner.next();
+
+        Files.createFile(Path.of("Homework28.txt"));
+
+        Files.write(Path.of("Homework28.txt"), txt.getBytes());
+
+        var inputList = Files.readAllLines(Path.of("Homework28.txt"));
+
+        System.out.println("Y`ve entered: " + inputList);
+
+    }
+
+}
