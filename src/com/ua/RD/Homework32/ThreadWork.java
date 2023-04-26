2486643ec17f96ab0091142e0fa855db36c7291a
bulldozzer96 <bulldozzer96@gmail.com>
2023-04-26 20:56:15 +0300

    Homework32

--- src/com/ua/RD/Homework32/ThreadWork.java
+++ src/com/ua/RD/Homework32/ThreadWork.java
@@ -3,7 +3,7 @@ package com.ua.RD.Homework32;
 public class ThreadWork implements Runnable {
     public void run() {
         try {
-            for (int i = 0; i < 10; i++) {
+            for (int i = 1; i < 11; i++) {
                 System.out.println("Result of " + Thread.currentThread().getName() + " is " + i);
 
             }
