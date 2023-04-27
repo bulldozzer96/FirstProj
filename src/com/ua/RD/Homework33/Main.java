6fc406ec547b2e6133b963a90dac65b576c87200
bulldozzer96 <bulldozzer96@gmail.com>
2023-04-27 21:21:38 +0300

    Homework33

---
+++ src/com/ua/RD/Homework33/Main.java
@@ -0,0 +1,21 @@
+package com.ua.RD.Homework33;
+
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        Thread t = new Thread(() -> {
+            int number;
+            do {
+                number = new Random().nextInt(1, 10);
+                System.out.println(Thread.currentThread().getName()+" generated " + number);
+            } while (number != 5);
+
+            System.out.println("Number 5 was found");
+        });
+        t.start();
+
+    }
+}
