f425da303ef9d7e2d4ab9424ccae0be15aa8299f
bulldozzer96 <bulldozzer96@gmail.com>
2023-05-01 20:09:43 +0300

    lesson34

---
+++ src/com/ua/RD/lesson34/Main.java
@@ -0,0 +1,23 @@
+package com.ua.RD.lesson34;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.*;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
+        List<Callable<String>> callables = new ArrayList<>();
+        for (int i = 0; i < 30; i++) {
+            callables.add(new TestCallable());
+        }
+
+        ExecutorService executorService = Executors.newFixedThreadPool(10);
+        List<Future<String>> futures = executorService.invokeAll(callables);
+        System.out.println("Start");
+        for (Future<String> future : futures) {
+            System.out.println(future.get());
+        }
+        System.out.println("Done");
+
+    }
+}
---
+++ src/com/ua/RD/lesson34/RestCallable.java
@@ -0,0 +1,11 @@
+package com.ua.RD.lesson34;
+
+import java.util.concurrent.Callable;
+
+public class RestCallable implements Callable<String> {
+    @Override
+    public String call() throws Exception {
+        return null;
+    }
+
+}
